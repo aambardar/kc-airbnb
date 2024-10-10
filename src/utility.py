5a405c567a5ea96e11b146202031e84f38f13963
aambardar <asheeshambardar@gmail.com>
2024-10-10 12:05:45 +1100

    Add column renaming function and update notebook imports
    Added `rename_columns` function in `utility.py` to rename DataFrame columns based on a dictionary. Updated `abnb_final_capstone_v0.8.ipynb` to reflect the new import and tested the renaming functionality. Simplified imports and fixed execution timing in the notebook.

--- src/utility.py
+++ src/utility.py
@@ -1,4 +1,5 @@
 import logger_setup
+import pandas as pd
 import re, os
 
 def pick_files_by_pattern(directory, pattern):
@@ -18,3 +19,28 @@ def add_prefix_to_columns(df: pd.DataFrame, prefix: str) -> pd.DataFrame:
     df.columns = new_columns
     logger_setup.logger.info('... FINISH')
     return df
+
+
+def rename_columns(df, columns_dict):
+    """
+    Renames columns of the DataFrame based on the provided dictionary.
+
+    Parameters:
+    df (pd.DataFrame): The original DataFrame.
+    columns_dict (dict): A dictionary where keys are current column names and values are new column names.
+
+    Returns:
+    pd.DataFrame: A new DataFrame with the renamed columns.
+    """
+    # Ensure input is a DataFrame
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("Input should be a pandas DataFrame")
+
+    # Ensure columns_dict is a dictionary
+    if not isinstance(columns_dict, dict):
+        raise ValueError("columns_dict should be a dictionary")
+
+    # Rename columns
+    new_df = df.rename(columns=columns_dict)
+
+    return new_df
